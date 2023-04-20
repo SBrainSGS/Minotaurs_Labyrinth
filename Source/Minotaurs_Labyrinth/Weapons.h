44a6f7d8735129bc181b42ae88f35914e62ad022
Aziz Salimov <salimov04@bk.ru>
2023-04-20 19:32:48 +0300

    Added some classes

---
+++ Source/Minotaurs_Labyrinth/Weapons.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MINOTAURS_LABYRINTH_API Weapons
+{
+public:
+	Weapons();
+	~Weapons();
+};
